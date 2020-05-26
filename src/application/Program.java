f6fac28dffbf6467e416cdb29860b234ec0b9bbf
TRexUliM <trexulim@gmail.com>
2020-05-26 16:55:28 -0300

    Vei, coloquei um UPDATE SEM WHERE

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,9 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
+		System.out.println("------------------------------");
 		System.out.println("Fala aí mundão véi sem portera");
+		System.out.println("------------------------------");
 
 	}
 
